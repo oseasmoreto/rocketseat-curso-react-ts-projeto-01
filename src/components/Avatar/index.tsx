3e5c08d880272b82487b4f1955763618913be320
oseasmoreto <oseas_moreto@hotmail.com>
2022-07-20 12:12:45 -0300

    Extendendo a interface do avatar dos atributos de img do html

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -7,13 +7,12 @@ interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?:boolean
 }
 
-export function Avatar({ hasBorder = true, src, alt} : AvatarProps){
+export function Avatar({ hasBorder = true, ...props} : AvatarProps){
 
   return (
     <img 
       className={ hasBorder ? styles.avatarWithBorder : styles.avatar} 
-      src={src} 
-      alt={alt} 
+      {...props} 
     />
   );
 }
